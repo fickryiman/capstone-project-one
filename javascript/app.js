0da3dc9a55782d6a817e19d96de6c3982ad10991
Fickry Iman <fickry.iman@gmail.com>
2023-03-16 01:02:06 +0700

    fixes speakers json

--- javascript/app.js
+++ javascript/app.js
@@ -1,44 +1,44 @@
 const speakers = [
   {
-    "name": "Fickry Bil Iman",
-    "image": "images/jpg/fickry-bil-iman.jpg",
-    "occupation": "Director of Art Centre Nabi and a board member of CC Indonesia",
-    "details": "As the main venue for new media art production in Indonesia, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts."
+    name: "Fickry Bil Iman",
+    image: "images/jpg/fickry-bil-iman.jpg",
+    occupation: "Director of Art Centre Nabi and a board member of CC Indonesia",
+    details: "As the main venue for new media art production in Indonesia, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts."
   },
   {
-    "name": "Fickry Bil Iman",
-    "image": "images/jpg/fickry-bil-iman.jpg",
-    "occupation": "Director of Art Centre Nabi and a board member of CC Indonesia",
-    "details": "As the main venue for new media art production in Indonesia, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts."
+    name: "Fickry Bil Iman",
+    image: "images/jpg/fickry-bil-iman.jpg",
+    occupation: "Director of Art Centre Nabi and a board member of CC Indonesia",
+    details: "As the main venue for new media art production in Indonesia, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts."
   },
   {
-    "name": "Fickry Bil Iman",
-    "image": "images/jpg/fickry-bil-iman.jpg",
-    "occupation": "Director of Art Centre Nabi and a board member of CC Indonesia",
-    "details": "As the main venue for new media art production in Indonesia, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts."
+    name: "Fickry Bil Iman",
+    image: "images/jpg/fickry-bil-iman.jpg",
+    occupation: "Director of Art Centre Nabi and a board member of CC Indonesia",
+    details: "As the main venue for new media art production in Indonesia, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts."
   },
   {
-    "name": "Fickry Bil Iman",
-    "image": "images/jpg/fickry-bil-iman.jpg",
-    "occupation": "Director of Art Centre Nabi and a board member of CC Indonesia",
-    "details": "As the main venue for new media art production in Indonesia, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts."
+    name: "Fickry Bil Iman",
+    image: "images/jpg/fickry-bil-iman.jpg",
+    occupation: "Director of Art Centre Nabi and a board member of CC Indonesia",
+    details: "As the main venue for new media art production in Indonesia, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts."
   },
   {
-    "name": "Fickry Bil Iman",
-    "image": "images/jpg/fickry-bil-iman.jpg",
-    "occupation": "Director of Art Centre Nabi and a board member of CC Indonesia",
-    "details": "As the main venue for new media art production in Indonesia, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts."
+    name: "Fickry Bil Iman",
+    image: "images/jpg/fickry-bil-iman.jpg",
+    occupation: "Director of Art Centre Nabi and a board member of CC Indonesia",
+    details: "As the main venue for new media art production in Indonesia, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts."
   },
   {
-    "name": "Fickry Bil Iman",
-    "image": "images/jpg/fickry-bil-iman.jpg",
-    "occupation": "Director of Art Centre Nabi and a board member of CC Indonesia",
-    "details": "As the main venue for new media art production in Indonesia, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts."
+    name: "Fickry Bil Iman",
+    image: "images/jpg/fickry-bil-iman.jpg",
+    occupation: "Director of Art Centre Nabi and a board member of CC Indonesia",
+    details: "As the main venue for new media art production in Indonesia, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts."
   },
   {
-    "name": "Fickry Bil Iman",
-    "image": "images/jpg/fickry-bil-iman.jpg",
-    "occupation": "Director of Art Centre Nabi and a board member of CC Indonesia",
-    "details": "As the main venue for new media art production in Indonesia, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts."
+    name: "Fickry Bil Iman",
+    image: "images/jpg/fickry-bil-iman.jpg",
+    occupation: "Director of Art Centre Nabi and a board member of CC Indonesia",
+    details: "As the main venue for new media art production in Indonesia, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts."
   }
 ]
